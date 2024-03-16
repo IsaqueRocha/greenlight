45ccecff5dd766d312dca56c9aaa8f27b3369b20
Isaque Rocha <isaquerocha@gmail.com>
2024-03-16 20:42:39 -0300

    feat: 3.1 -- add json encoding to healthcheck

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,9 @@ import (
 )
 
 func (a *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintln(w, "environment:", a.config.env)
-	fmt.Fprintln(w, "version:", version)
+	js := `{"staus":"available", "environment": %q, "version":%q}`
+	js = fmt.Sprintf(js, a.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(js))
 }
